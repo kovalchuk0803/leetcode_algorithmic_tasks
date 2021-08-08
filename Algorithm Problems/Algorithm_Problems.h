5f62adbd4a2febd3c34d072aff0284627bda2e0d
Ilya Kovalchuk <i.kovalchuk@adguard.com>
2021-08-08 17:40:52 +0300

    Initial Commit

---
+++ Algorithm Problems/Algorithm_Problems.h
@@ -0,0 +1,18 @@
+//
+//  Algorithm_Problems.h
+//  Algorithm Problems
+//
+//  Created by Илья Ковальчук on 08.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Algorithm_Problems.
+FOUNDATION_EXPORT double Algorithm_ProblemsVersionNumber;
+
+//! Project version string for Algorithm_Problems.
+FOUNDATION_EXPORT const unsigned char Algorithm_ProblemsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algorithm_Problems/PublicHeader.h>
+
+
